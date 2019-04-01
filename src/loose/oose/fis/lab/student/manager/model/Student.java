d7dfecd8289615e0ae3bfa8dc1e74f62a142b13f
flavi08 <matheiuflavius@gmail.com>
2019-04-01 12:31:15 +0300

    SM-2 Implemented the Student model class

---
+++ src/loose/oose/fis/lab/student/manager/model/Student.java
@@ -0,0 +1,69 @@
+package loose.oose.fis.lab.student.manager.model;
+
+import java.util.Objects;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double meanGrade;
+
+    public Student(String firstName, String lastName, int age, double meanGrade) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.meanGrade = meanGrade;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMeanGrade() {
+        return meanGrade;
+    }
+
+    public void setMeanGrade(double meanGrade) {
+        this.meanGrade = meanGrade;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Student)) return false;
+        Student student = (Student) o;
+        return age == student.age;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(age);
+    }
+}
